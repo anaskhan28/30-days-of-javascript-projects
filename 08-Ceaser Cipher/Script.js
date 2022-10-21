cb83f6f320f93f2fa7878c1a3b76875602e51ab3
anaskhan28 <khananasatique@gmail.com>
2022-10-21 23:44:48 +0530

    not completed

--- 08-Ceaser Cipher/Script.js
+++ 08-Ceaser Cipher/Script.js
@@ -3,10 +3,15 @@ let eInput = document.getElementById('encryptedInput-1'); // encrypted input
 let pInput = document.getElementById('plainInput-1') // plain input text
 let inputs = [eInput,pInput]
 let copyBtn= document.getElementById('copyToClipBoard');
+let shiftInput = parseInt(document.getElementById('shiftInput').value)
+ 
 
-inputs.forEach(input=>{
-    input.oninput=()=>{
-        input.value = input.value.toUppserCase();
-        console.log('anas')
+
+function Encrypt() {
+    let solved ='';
+    for(let i=0; i<pInput.length; i++){
+        sum >=65 && sum <=90 ? solved +=String.fromCharCode(sum): sum>90 ? solved +=String.fromCharCode(65+(sum&91)):solved+=pInput[i]
     }
-})
+    console.log(eInput.value =solved);
+}
+encryptBtn.addEventListener('click', Encrypt)
