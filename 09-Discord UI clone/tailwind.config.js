b405a8ddf5e59a05c2cef3707ae676725bcefae2
anaskhan28 <khananasatique@gmail.com>
2022-10-23 14:37:48 +0530

    header part is done  and body section

--- 09-Discord UI clone/tailwind.config.js
+++ 09-Discord UI clone/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center:true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors:{
+        primary:"#404eed",
+        black: "#23272a",
+      },
+    },
   },
   plugins: [],
 }
